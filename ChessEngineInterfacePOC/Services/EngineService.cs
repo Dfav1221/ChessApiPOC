c6618c873f6a8658cc8656cf66541313fc06704e
JakubHirsz <jakub.hirsz2000@gmail.com>
2022-03-04 21:18:21 +0100

    Changed Process creation, now engine can be called multiple times :))

--- ChessEngineInterfacePOC/Services/EngineService.cs
+++ ChessEngineInterfacePOC/Services/EngineService.cs
@@ -2,6 +2,8 @@
 using System.Text;
 using ChessEngineInterfacePOC.Interfaces;
 using ChessEngineInterfacePOC.Models;
+using CliWrap;
+using CliWrap.EventStream;
 using Microsoft.Extensions.Options;
 
 namespace ChessEngineInterfacePOC.Services;
@@ -15,33 +17,33 @@ public class EngineService : IEngineService
     public EngineService(IOptions<EngineConfig> options)
     {
         _options = options;
+        _engineProcess = new Process();
+        _engineProcess.StartInfo.FileName = "C:\\Users\\JakubHirsz\\Desktop\\stockfish_14.1_win_x64_avx2\\ryba.exe";
+        _engineProcess.StartInfo.RedirectStandardInput = true;
+        _engineProcess.StartInfo.RedirectStandardOutput = true;
+        _engineProcess.StartInfo.UseShellExecute = false;
+        _engineProcess.StartInfo.CreateNoWindow = true;
     }
 
     public async Task<string> CalculatePosition(string position, int time, char color)
     {
-        _engineProcess = new Process();
-        _engineProcess.StartInfo.UseShellExecute = false;
-        _engineProcess.StartInfo.RedirectStandardInput = true;
-        _engineProcess.StartInfo.RedirectStandardOutput = true;
-        _engineProcess.StartInfo.FileName = _options.Value.Path;
-        _engineProcess.StartInfo.CreateNoWindow = false;
         _engineProcess.Start();
+
         await using (var writer = _engineProcess.StandardInput)
         {
-            await writer.WriteAsync("uci");
-            await writer.WriteLineAsync($"position {position}");
-            await writer.WriteLineAsync($"go wtime {time}");
+            await writer.WriteAsync($"position {position}\r\ngo wtime {time}");
         }
-        
+
         var response = "";
         using (var reader = _engineProcess.StandardOutput)
         {
             var output = await reader.ReadToEndAsync();
-            response = output.Split("bestmove ")[1];
+            response = output
+                .Split("bestmove ")[1]
+                .Split('\r')[0];
         }
 
-        await _engineProcess.WaitForExitAsync();
-        
+
         return response;
     }
 }
